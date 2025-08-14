b09b6210c41ee284cc439fcdd70c8079f5697d9b
ChurinDmitriy <mister.dim2017111@gmail.com>
2025-08-15 00:47:30 +0500

    add tests

--- src/test/java/pages/PageWithHome.java
+++ src/test/java/pages/PageWithHome.java
@@ -1,4 +1,126 @@
 package pages;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class PageWithHome {
+
+    private final SelenideElement
+            cookieBtn = $(".cookie-agreement").$(byTagAndText("button", "Хорошо")),
+            header = $(".header"),
+            mainBanner = $(".section-slider"),
+            categories = $(".section-categories"),
+            bestsellers = $(".section-brands"),
+            couldBeUseful = $("#gf-wrapper-recs-gf-hp-1"),
+            popularItems = $("#gf-wrapper-recs-gf-hp-2"),
+            sectionEcosystem = $(".section-ecosystem"),
+            sectionBlog = $(".section-products"),
+            lowestFooter = $(".prerender-footer"),
+            chatPage = $("[component='ChatTab']"),
+            product = $(".product-card--hovered"),
+            bookingPage = $(".popmechanic-desktop");
+
+
+
+
+    @Step("Открыть главную страницу")
+    public PageWithHome openPage() {
+        open("");
+        Selenide.sleep(4500);
+        return this;
+    }
+
+    @Step("Закрыть Cookie")
+    public PageWithHome closeCookie() {
+        if (cookieBtn.isDisplayed()) {
+            cookieBtn.click();
+        }
+        return this;
+    }
+
+    @Step("Проверка элементов верхнего блока")
+    public PageWithHome checkTopHeader(String element) {
+        header.shouldHave(text(element));
+        return this;
+    }
+
+    @Step("Проверка отображения главного баннера")
+    public PageWithHome checkMainBanner() {
+        mainBanner.shouldBe(visible);
+        return this;
+    }
+
+    @Step("Проверка отображения категорий товаров")
+    public PageWithHome checkCategories(String element) {
+        categories.shouldHave(text(element));
+        return this;
+    }
+
+    @Step("Проверка отображения блока бестселлеров")
+    public PageWithHome checkBestsellers(String element) {
+        bestsellers.shouldHave(text(element));
+        return this;
+    }
+
+    @Step("Проверка отображения блока 'Вам может пригодиться'")
+    public PageWithHome checkCouldBeUseful(String element) {
+        couldBeUseful.shouldHave(text(element));
+        return this;
+    }
+
+    @Step("Проверка отображения блока популярных товаров")
+    public PageWithHome checkPopularItems(String element) {
+        popularItems.shouldHave(text(element));
+        return this;
+    }
+
+    @Step("Проверка отображения блока экосистемы")
+    public PageWithHome checkSectionEcosystem(String element) {
+        sectionEcosystem.shouldHave(text(element));
+        return this;
+    }
+
+    @Step("Проверка отображения блока блога")
+    public PageWithHome checkSectionBlog(String element) {
+        sectionBlog.shouldHave(text(element));
+        return this;
+    }
+
+    @Step("Проверка отображения нижнего футера")
+    public PageWithHome checkLowestFooter(String element) {
+        lowestFooter.shouldHave(text(element));
+        return this;
+    }
+
+    @Step("Проверка отображения элементов чата")
+    public PageWithHome checkChatItems(String word) {
+        $(".Uq2nE").shouldHave(text(word));
+        return this;
+    }
+
+    @Step("Проверка отображения элементов диалога")
+    public PageWithHome checkChatPage(String word) {
+        chatPage.shouldHave(text(word));
+        return this;
+    }
+
+    @Step("Выбрать продукт")
+    public PageWithHome chooseProduct(String word) {
+        product.click();
+        return this;
+    }
+
+    @Step("Проверка элементов страницы оформления заказа")
+    public PageWithHome checkBookingPage(String word) {
+        bookingPage.shouldHave(text(word));
+        return this;
+    }
+
 }
--- src/test/java/tests/HomeTests.java
+++ src/test/java/tests/HomeTests.java
@@ -1,4 +1,285 @@
 package tests;
 
-public class HomeTests {
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.PageWithHome;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
+import static io.qameta.allure.Allure.step;
+
+@Tag("regression")
+public class HomeTests extends TestBase {
+
+    PageWithHome steps = new PageWithHome();
+
+    @Test
+    @DisplayName("Проверка входа в аккаунт через электронную почту")
+    @Tag("smoke")
+    @Owner("ChurinDmitiy")
+    void loginEmailTest() {
+        steps.openPage();
+        steps.closeCookie();
+        step("Открыть вход в аккаунт", () -> {
+            $(".header-icons__item").click();
+        });
+        step("Перейти к форме входа в аккаунт", () -> {
+            $(".header-dropdown-lk__btn").click();
+        });
+        step("Войти с помощью электронной почты", () -> {
+            $(byTagAndText("button", "Войти по почте")).click();
+        });
+        step("ввести электронную почту", () -> {
+            $("[placeholder='Эл. почта']").setValue("mister.dim2017111@gmail.com");
+        });
+        step("Войти в аккаунт", () -> {
+            $(".form-auth__button").$(byTagAndText("button", "Войти")).shouldBe(visible);
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка входа в аккаунт через номер телефона")
+    @Tag("smoke")
+    @Owner("ChurinDmitiy")
+    void loginNumberTest() {
+        steps.openPage();
+        steps.closeCookie();
+        step("Открыть вход в аккаунт", () -> {
+            $(".header-icons__item").click();
+        });
+        step("Перейти к форме входа в аккаунт", () -> {
+            $(".header-dropdown-lk__btn").click();
+        });
+        step("Ввести номер телефона", () -> {
+            $("[placeholder='Телефон']").setValue("+7 (932) 437-21-74");
+        });
+        step("Войти в аккаунт", () -> {
+            $(".form-auth__button").$(byTagAndText("button", "Войти")).shouldBe(visible);
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка отображения элементов главной страницы")
+    @Tag("smoke")
+    @Owner("ChurinDmitiy")
+    void homeTest() {
+        steps.openPage();
+        steps.closeCookie();
+        steps.checkTopHeader("Магазины");
+        steps.checkTopHeader("Сервисные центры");
+        steps.checkTopHeader("Бизнесу");
+        steps.checkTopHeader("Блог");
+        steps.checkTopHeader("Live ◎");
+        steps.checkTopHeader("restore:u");
+        steps.checkTopHeader("Доставка и оплата");
+        steps.checkTopHeader("Где мой заказ?");
+
+        steps.checkTopHeader("SMART-уход");
+        steps.checkTopHeader("Привилегии");
+        steps.checkTopHeader("New");
+        steps.checkTopHeader("Sale");
+
+        steps.checkTopHeader("Apple");
+        steps.checkTopHeader("Смартфоны и гаджеты");
+        steps.checkTopHeader("Компьютеры и ноутбуки");
+        steps.checkTopHeader("ТВ, аудио и видео");
+        steps.checkTopHeader("Для дома");
+        steps.checkTopHeader("Красота и здоровье");
+        steps.checkTopHeader("Развлечения");
+        steps.checkTopHeader("Путешествия и спорт");
+        steps.checkTopHeader("Аксессуары");
+
+        steps.checkTopHeader("Ваш город");
+
+        steps.checkMainBanner();
+
+        steps.checkCategories("Apple");
+        steps.checkCategories("Смартфоны и гаджеты");
+        steps.checkCategories("Компьютеры и ноутбуки");
+        steps.checkCategories("ТВ, аудио и видео");
+        steps.checkCategories("Для дома");
+        steps.checkCategories("Красота и здоровье");
+        steps.checkCategories("Развлечения");
+        steps.checkCategories("Аксессуары");
+
+        steps.checkCouldBeUseful("Вам может пригодиться");
+
+        steps.checkPopularItems("Популярные товары");
+
+        steps.checkSectionEcosystem("restore:");
+        steps.checkSectionEcosystem("Больше, чем просто магазин устройств");
+        steps.checkSectionEcosystem("Почему restore:?");
+        steps.checkSectionEcosystem("Покупайте онлайн и в розничных магазинах restore:");
+        steps.checkSectionEcosystem("Купить онлайн");
+        steps.checkSectionEcosystem("Найти магазины");
+        steps.checkSectionEcosystem("restore:care");
+        steps.checkSectionEcosystem("restore:business");
+        steps.checkSectionEcosystem("Найти магазины");
+
+        steps.checkSectionBlog("Блог");
+        steps.checkSectionBlog("Читай, смотри, действуй");
+        steps.checkSectionBlog("Все статьи");
+        steps.checkSectionBlog("Блог");
+        steps.checkSectionBlog("Блог");
+        steps.checkSectionBlog("Блог");
+
+        steps.checkLowestFooter("Будьте в курсе последних новостей и акций");
+        steps.checkLowestFooter("Ваш email");
+        steps.checkLowestFooter("Отправить");
+        steps.checkLowestFooter("О компании");
+        steps.checkLowestFooter("Помощь");
+        steps.checkLowestFooter("Каталог");
+        steps.checkLowestFooter("iPhone");
+        steps.checkLowestFooter("8 800 700-19-44");
+        steps.checkLowestFooter("+7 495 502-15-52 (для бизнеса)");
+        steps.checkLowestFooter("© restore:, 2025");
+    }
+
+    @Test
+    @DisplayName("Проверка работы чата")
+    @Tag("smoke")
+    @Owner("ChurinDmitiy")
+    void chatTest() {
+        steps.openPage();
+        steps.closeCookie();
+        Selenide.sleep(4500);
+        step("Открыть чат", () -> {
+            $("[name='flomni:toggler']").click();
+        });
+        switchTo().frame("flomni:pager"); // Переход внтурь дерева чата
+        steps.checkChatItems("Привет!");
+        steps.checkChatItems("Напишите нам, мы любим общаться");
+        steps.checkChatItems("Новый чат");
+        steps.checkChatItems("Написать в мессенджер");
+        step("Переход в чат", () -> {
+            $("[type='StartMessagingButton']").click();
+
+        });
+        steps.checkChatPage("Начиная или продолжая общение в чате, вы соглашаетесь c обработкой ваших персональных данных");
+        steps.checkChatPage("Соглашаюсь");
+        step("Проверка отображения поля ввода", () -> {
+            $("[placeholder='Введите сообщение']").shouldBe(visible);
+        });
+        step("Проверка отображения кнопки отправления сообщения", () -> {
+            $("._7vjvu.ZOca1").shouldBe(visible);
+        });
+        step("Закрыть чат", () -> {
+            $("[element='Collapse']").click();
+        });
+        step("Завершение диалога", () -> {
+            $(byTagAndText("div", "Завершить диалог")).click();
+        });
+        step("Выбор оценки общения", () -> {
+            $("[component='CloseChatStep2']").$("._7vjvu").sibling(3).click();
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка покупки ноутбука")
+    @Tag("smoke")
+    @Owner("ChurinDmitiy")
+    void e2eBuyMacBookTest() {
+        steps.openPage();
+        steps.closeCookie();
+        step("Выбрать категорию 'Компьютеры и ноутбуки'", () -> {
+            $("[data-campaign-id='kompyutery-i-noutbuki']").click();
+        });
+        step("Выбрать ноутбук Apple", () -> {
+            $(byTagAndText("div", "Ноутбуки")).closest("div").$(byText("Apple")).click();
+        });
+        step("Выбрать категорию Macbook Air", () -> {
+            $(".fast-filter").$(byText("Macbook Air")).click();
+        });
+        steps.chooseProduct("MacBook Air");
+        step("Добавить в избранное", () -> {
+            $(".favorite__button").click();
+        });
+        step("Перейти в избранное", () -> {
+            $(".header-icons__item").sibling(0).click();
+        });
+        steps.chooseProduct("MacBook Air");
+        step("Добавить в корзину", () -> {
+            $(byTagAndText("button", "Добавить в корзину")).click();
+        });
+        step("Закрыть предложенные товары", () -> {
+            $(".modal-box__close").click();
+        });
+        step("Перейти в корзину", () -> {
+            $(".header-icons__item").sibling(2).click();
+        });
+        step("Перейти к оформлению", () -> {
+            $("#cartButton").click();
+        });
+        steps.checkBookingPage("Оформление заказа");
+        steps.checkBookingPage("Телефон");
+        steps.checkBookingPage("Имя");
+        steps.checkBookingPage("E-mail");
+        steps.checkBookingPage("Способ получения");
+        steps.checkBookingPage("Способ оплаты");
+        steps.checkBookingPage("При получении");
+        steps.checkBookingPage("Онлайн-оплата");
+        steps.checkBookingPage("Кредит");
+        steps.checkBookingPage("Перезвоните мне для подтверждения заказа");
+        steps.checkBookingPage("Оформить заказ");
+
+    }
+
+    @Test
+    @DisplayName("Проверка работы поиска")
+    @Tag("smoke")
+    @Owner("ChurinDmitiy")
+    void searchTest() {
+        steps.openPage();
+        steps.closeCookie();
+        step("Нажать на поле поиска", () -> {
+            $(".search-line").click();
+        });
+        step("Ввести запрос в поиск", () -> {
+            $("[placeholder=' Что вы ищете?']").setValue("dyson").pressEnter();
+        });
+        step("Проверка корректного отображения результата поиска предмета", () -> {
+            $(".search-params__query").shouldHave(text("dyson"));
+        });
+        step("Проверка отображения предмета в каталоге", () -> {
+            $(".catalog__products").shouldHave(text("dyson"));
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка добавления товаров в сравнение")
+    @Tag("smoke")
+    @Owner("ChurinDmitiy")
+    void compareTest() {
+        steps.openPage();
+        steps.closeCookie();
+        step("Выбрать категорию 'Apple'", () -> {
+            $("[data-campaign-id='apple']").click();
+        });
+        step("Выбрать категорию iPhone", () -> {
+            $(".categories__item").click();
+        });
+        step("Выбрать первый iPhone", () -> {
+            $(".product-card--hovered").$(".product-card__icon").click();
+        });
+        step("Выбрать второй iPhone", () -> {
+            $(".product-card--hovered").sibling(0).$(".product-card__icon").click();
+        });
+        step("Перейти в сравнение товаров", () -> {
+            $(".header-icons__item").sibling(1).click();
+        });
+        step("Проверка перехода в сравнение товаров", () -> {
+            $(".compare__header").shouldHave(text("Сравнение товаров"));
+        });
+        step("Проверка отображения двух iPhone в сравнение товаров", () -> {
+            $(".products__slide").shouldHave(text("IPhone"));
+            $(".products__slide").sibling(0).shouldHave(text("IPhone"));
+        });
+    }
 }
